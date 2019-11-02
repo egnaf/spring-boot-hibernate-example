03ef8fbecd6dbe7c86a1b368c05317505f134350
Sherzod Mamadaliev <egnaf@yahoo.com>
2019-11-02 16:24:39 +0300

    Add basic spring boot application #1

---
+++ any-to-any/src/main/java/com/company/app/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.company.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
